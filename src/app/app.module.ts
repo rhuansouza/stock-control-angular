f3b70a7396a3b0879d35402f9a9abf655f933459
Rhuan Costanzo <rhuan@cooxupe.com>
2023-08-18 17:19:45 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './modules/home/home.component';
+// Importações PrimeNg
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    //PrimeNg
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  loginCard = true;
+
+  loginForm = this.formBuilder.group({
+    email:['', Validators.required],
+    password:['',Validators.required]
+  })
+
+  constructor(private formBuilder: FormBuilder){
+
+  }
+}
